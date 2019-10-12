bd11d15e9a7fc4c52c514f2b38903a8e3562d2df
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-10-11 18:22:08 -0700

    Create readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Psychic-Game
+# Psychic Game
+
+# Introduction
+
+I built a guessing game built using JavaScript. The instructions are below along with a GIF. I was able to add in a method which only allows the player to press a key from a-z and I added in a caps lock override. 
+
+ ## Summary
+
+This was my first JavaScript project. 
+
+ ## Technologies Used
+ - HTML
+ - CSS
+ - Git
+ - Github
+ - JavaScript
+
+ ## Instructions
+
+ ##### How to play:
+
+ - Select a letter a-z
+ - You have 10 guesses to get it right
+ - If you get it right, the game automatically restarts and your wins goes up by 1
+ - If you go through all 10 guesses your losses will go up by 1
+
+![Psychic-Game](assets/images/psychic-game.gif)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,69 @@
 
 //Make an array for computer choices
-var computerChoice = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 
 //Make variable for win, lose,  guesses left, user guess and computer guess
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var letterGuessed = [];
+// var previousGuess = [];
 
 //Retrieving the id's from index.html
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var numOfGuessText = document.getElementById("numofguess-text");
 var userGuessesText = document.getElementById("userguesses-text");
- 
+
 //Generating computer random letter
 var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
 //Uses variable computerGuess to select random letter from array computerChoice
-function compGuess(){
+function compGuess() {
     computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
-console.log(computerGuess);
 }
 //Call on function compGuess
 compGuess();
 
- //The game....
- //,onkeyup captures players input or in this case event
-document.onkeyup = function(event){
-//Added .toLowerCase() to make the game playable even when caps lock is on
-    var userGuess = event.key.toLowerCase();
-    letterGuessed.push(userGuess);
-        console.log(userGuess);
+//The game....
+//,onkeyup captures players input or in this case event
+document.onkeyup = function (event) {
+    //Alerted when a key other than lowercase a-z was pressed
+    if (event.keyCode < 65 || event.keyCode > 90) {
+        alert("Invaild")
 
-//Guesses will go down after user presses a buttom
-    guessesLeft --;
+    } else  {
+        //Added .toLowerCase() to make the game playable even when caps lock is on
+        var userGuess = event.key.toLowerCase();
+        letterGuessed.push(userGuess);
+        //Guesses will go down after user presses a buttom
+        guessesLeft--;
+    }
 
-//Alerted when a key other than lowercase a-z was pressed
-//Not able to find where to place it, because placing it here did coninuted to the character in the letterGuessed
-// if (event.keyCode < 65 || event.keyCode > 90){
-//     alert("Invaild")
-//  }
+    //Initating a win scenario
+    if ((userGuess === computerGuess) && (guessesLeft > 0)) {
+        wins++;
+        restart();
 
-
-//Initating a win scenario
-    if((userGuess === computerGuess) && (guessesLeft > 0)){
-         wins++;
-         restart();
-  
 
     //Initiating a lose scenario
-    }else if(guessesLeft == 0){
+    } else if (guessesLeft == 0) {
         losses++;
         restart();
-    }   
+    }
     
-winsText.textContent = "Wins: " + wins;
-lossesText.textContent = "Losses: " + losses;
-numOfGuessText.textContent = "Guesses left: " + guessesLeft;
-userGuessesText.textContent = "Your guesses so far: " + letterGuessed;
+    
+       
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    numOfGuessText.textContent = "Guesses left: " + guessesLeft;
+    userGuessesText.textContent = "Your guesses so far: " + letterGuessed;
 
 };
 
 //Function to restart  the game after 10 guesses or a win
-var restart = function(){
+var restart = function () {
     guessesLeft = 10;
     letterGuessed = [];
     compGuess();
-};
-
-
-
-
-
+}
